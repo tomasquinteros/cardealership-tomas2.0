05fb3d3e774484dfd9dd9c6ac96a8c7b401efbd9
Tomas <tomassaidquinteros@gmail.com>
2022-12-15 20:48:40 -0300

    Hola

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useState, useRef } from "react";
 import {FaBars, FaTimes} from 'react-icons/fa';
 
