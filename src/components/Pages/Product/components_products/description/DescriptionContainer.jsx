109c52dabeacd7724ec6ed84f81a36cb91e00bf2
Tomas Quinteros <tomassaidquinteros@gmail.com>
2023-01-07 18:40:49 -0300

    add link "back to model" in description

--- src/components/Pages/Product/components_products/description/DescriptionContainer.jsx
+++ src/components/Pages/Product/components_products/description/DescriptionContainer.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import '../../../../../styles/description.scss'
 
 const DescriptionContainer = ({details, car}) => {
@@ -15,39 +16,42 @@ const DescriptionContainer = ({details, car}) => {
 
    return (
       <div className="description">
-         <div id={id} className="container max-content">
-            <section className="img-container">
-               <img src={currentImage} alt=""  className="big-img"/>
-               <div className="thumbnail-img">
-                  {
-                     img.map((image, index) => (
-                        <img key={index} src={image} alt={index} 
-                        onClick={() => {handleImage(index)}}
-                        className={active === index ? 'img active' : 'img'}
-                        />
-                        ))
-                     }
-               </div>
-            </section>
-            <section className="description-details">
-                  <h1>{make} {model}</h1>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo fuga qui temporibus ducimus impedit modi minus.</p>
-                  <div className="details">
-                     <h2>DETAILS</h2>
-                     <ul>
-                        <li><strong className="title">Class:</strong> {classes}</li>
-                        <li><strong className="title">Miles for galon in City: </strong>{city_mpg}</li>
-                        <li><strong className="title">Miles for galon in Highway:</strong> {highway_mpg}</li>
-                        <li><strong className="title">Fuel type:</strong> {fuel_type}</li>
-                        <li><strong className="title">Drive: </strong>{drive}</li>
-                        <li><strong className="title">Transmission: </strong>{transmission.toUpperCase()}</li>
-                        <li><strong className="title">Displacement: </strong>{displacement}</li>
-                        <li><strong className="title">Cylinders: </strong>{cylinders}</li>
-                     </ul>
-                     <h2 className="price">Price : ${price} </h2>
+         <div id={id} className="content max-content">
+            <Link to={"/models"} className="back-to-models">Back to models</Link>
+            <div className="container">
+               <section className="img-container">
+                  <img src={currentImage} alt=""  className="big-img"/>
+                  <div className="thumbnail-img">
+                     {
+                        img.map((image, index) => (
+                           <img key={index} src={image} alt={index} 
+                           onClick={() => {handleImage(index)}}
+                           className={active === index ? 'img active' : 'img'}
+                           />
+                           ))
+                        }
                   </div>
-                     
-            </section>
+               </section>
+               <section className="description-details">
+                     <h1>{make} {model}</h1>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo fuga qui temporibus ducimus impedit modi minus.</p>
+                     <div className="details">
+                        <h2>DETAILS</h2>
+                        <ul>
+                           <li><strong className="title">Class:</strong> {classes}</li>
+                           <li><strong className="title">Miles for galon in City: </strong>{city_mpg}</li>
+                           <li><strong className="title">Miles for galon in Highway:</strong> {highway_mpg}</li>
+                           <li><strong className="title">Fuel type:</strong> {fuel_type}</li>
+                           <li><strong className="title">Drive: </strong>{drive}</li>
+                           <li><strong className="title">Transmission: </strong>{transmission.toUpperCase()}</li>
+                           <li><strong className="title">Displacement: </strong>{displacement}</li>
+                           <li><strong className="title">Cylinders: </strong>{cylinders}</li>
+                        </ul>
+                        <h2 className="price">Price : ${price} </h2>
+                     </div>
+                        
+               </section>
+            </div>
          </div>
       </div>
    )
